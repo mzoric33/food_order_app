2e2a3e35340da2f6ca999b3ee849116dc21e1b13
mzoric33 <zoricmarin4@gmail.com>
2022-09-23 18:24:59 +0200

    cart_modal_state

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,7 +2,7 @@ import classes from "./Modal.module.css";
 import ReactDom from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop}></div>;
+  return <div className={classes.backdrop} onClick={props.onHideCart}></div>;
 };
 
 const ModalOverlay = (props) => {
@@ -18,7 +18,10 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDom.createPortal(<Backdrop />, portalElement)}
+      {ReactDom.createPortal(
+        <Backdrop onHideCart={props.onHideCart} />,
+        portalElement
+      )}
       {ReactDom.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
