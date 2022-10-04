986a27e5fae6934efcdb7e4ddf77e569935cac14
mzoric33 <zoricmarin4@gmail.com>
2022-10-04 21:05:46 +0200

    handling_http_errors

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -6,15 +6,22 @@ import { useEffect, useState } from "react";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://food-order-app-6f129-default-rtdb.firebaseio.com/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const data = await response.json();
 
       const loadedMeals = [];
+
       for (const key in data) {
         loadedMeals.push({
           id: key,
@@ -26,13 +33,21 @@ const AvailableMeals = () => {
       setMeals(loadedMeals);
       setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
+    return <section className={classes.spinner}></section>;
+  }
+
+  if (httpError) {
     return (
-      <section className={classes.spinner}>
-        <p>Loading...</p>
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
